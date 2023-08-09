c97145196e63fa8257f8fd93ec6f05ca01d379dd
DamarisCaballero <damariscallero011@gmail.com>
2023-08-09 15:54:21 -0600

    add

--- react-course/src/Components/Navbar/index.jsx
+++ react-course/src/Components/Navbar/index.jsx
@@ -7,8 +7,8 @@ const Navbar = () => {
     <nav className='flex justify-between items-center fixed z-10 w-full py-5 px-8 text-sm font-light'>
         <ul className='flex items-center gap-3'>
         <li className='font-semibold text-lg'>
-            <NavLink to='/'>
-            Shopi
+            <NavLink  to='/'> 
+        Shopi
             </NavLink>
         </li>
         <li>
@@ -68,7 +68,7 @@ const Navbar = () => {
         </ul>
             <ul className='flex items-center gap-3'>
             <li className='text-black/60'>
-            teff@platzi.com
+            Damaris@gmail.com
             </li>
             <li>
             <NavLink
