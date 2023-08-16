80ccdd82f4530b04525d085032870c1ac8f8ada2
DamarisCaballero <damariscallero011@gmail.com>
2023-08-16 13:45:10 -0600

    change

--- react-course/src/Components/Card/index.jsx
+++ react-course/src/Components/Card/index.jsx
@@ -2,12 +2,18 @@ import { useContext } from 'react'
 import { PlusIcon } from '@heroicons/react/24/solid'
 import { ShoppingCartContext } from '../../Context'
 
-    const Card = (data) => {
+const Card = (data) => {
     const context = useContext(ShoppingCartContext)
 
     const showProduct = (productDetail) => {
         context.openProductDetail()
         context.setProductToShow(productDetail)
+    }
+
+    const addProductsToCart = (productData) => {
+        context.setCount(context.count + 1)
+        context.setCartProducts([...context.cartProducts, productData])
+        console.log('CART: ', context.cartProducts)
     }
 
     return (
@@ -19,7 +25,7 @@ import { ShoppingCartContext } from '../../Context'
             <img className='w-full h-full object-cover rounded-lg' src={data.data.images[0]} alt={data.data.title} />
             <div
             className='absolute top-0 right-0 flex justify-center items-center bg-white w-6 h-6 rounded-full m-2 p-1'
-            onClick={() => context.setCount(context.count + 1)}>
+            onClick={() => addProductsToCart(data.data)}>
             <PlusIcon className='h-6 w-6 text-black'></PlusIcon>
             </div>
         </figure>
