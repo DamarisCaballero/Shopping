81cc3d68f01acc3ac09aed996ec9b7d642f38ddc
DamarisCaballero <damariscallero011@gmail.com>
2023-08-16 10:58:50 -0600

    add

--- react-course/src/Components/Card/index.jsx
+++ react-course/src/Components/Card/index.jsx
@@ -2,11 +2,18 @@ import { useContext } from 'react'
 import { PlusIcon } from '@heroicons/react/24/solid'
 import { ShoppingCartContext } from '../../Context'
 
-const Card = (data) => {
+    const Card = (data) => {
     const context = useContext(ShoppingCartContext)
 
+    const showProduct = (productDetail) => {
+        context.openProductDetail()
+        context.setProductToShow(productDetail)
+    }
+
     return (
-        <div className='bg-white cursor-pointer w-56 h-60 rounded-lg'>
+        <div
+        className='bg-white cursor-pointer w-56 h-60 rounded-lg'
+        onClick={() => showProduct(data.data)}>
         <figure className='relative mb-2 w-full h-4/5'>
             <span className='absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-xs m-2 px-3 py-0.5'>{data.data.category.name}</span>
             <img className='w-full h-full object-cover rounded-lg' src={data.data.images[0]} alt={data.data.title} />
@@ -22,6 +29,6 @@ const Card = (data) => {
         </p>
         </div>
     )
-}
+    }
 
 export default Card
