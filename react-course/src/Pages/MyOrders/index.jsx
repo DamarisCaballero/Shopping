2ab1ccfa273b8a4c96c7e72cb62889d142e572e0
DamarisCaballero <damariscallero011@gmail.com>
2023-08-24 16:12:40 -0600

    add

--- react-course/src/Pages/MyOrders/index.jsx
+++ react-course/src/Pages/MyOrders/index.jsx
@@ -13,16 +13,16 @@ function MyOrders() {
             <h1 className='font-medium text-xl'>My Orders</h1>
         </div>
         {
-            context.order.map((order, index) => {
+            context.order.map((order, index) => (
             <Link key={index} to={`/my-orders/${index}`}>
                 <OrdersCard
                 totalPrice={order.totalPrice}
                 totalProducts={order.totalProducts} />
             </Link>
-            })
+            ))
         }
         </Layout>
     )
-    }
+}
 
 export default MyOrders
